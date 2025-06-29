bf5b04b512d7a7fd5bf7b56bb65085a9b4e5676a
Goodwillhakim <goodwillhakim9@gmail.com>
2025-06-29 08:42:33 -0700

    NEW

---
+++ Kalkulator Persegi.py
@@ -0,0 +1,13 @@
+print("=" *41)
+print("           Kalkulator Persegi")
+print("=" *41)
+
+#INPUT 
+
+Panjang_Sisi=float(input("Masukan Panjang Sisi: "))
+
+#OUTOUT
+
+Luas = Panjang_Sisi * Panjang_Sisi
+Keliling = 4 * Panjang_Sisi 
+print(f"Jadi Luas = {Luas}, Keliling = {Keliling}")
